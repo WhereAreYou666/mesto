6bce9b0f50373dfb5f58966a13a91082d056e91a
Ruslan Poluyanov <monsterl33t@gmail.com>
2023-03-23 04:22:22 +0300

    fix изменить CSS, поправить HTML

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
 const editProfile = document.querySelector('.profile__edit-button');
 const editProfilePopup = document.querySelector('.popup_edit-profile');
 const editProfilePopupCloseButton = editProfilePopup.querySelector('.popup__close-button');
-const nameInput = editProfilePopup.querySelector('.popup__input-name');
-const descriptionInput = editProfilePopup.querySelector('.popup__input-description');
+const nameInput = editProfilePopup.querySelector('.popup__input_type_name');
+const descriptionInput = editProfilePopup.querySelector('.popup__input_type_description');
 const editProfilePopupSubmit = editProfilePopup.querySelector('.popup__submit-button');
 const editProfilePopupForm = editProfilePopup.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
 editProfile.addEventListener('click', () => {
-    editProfilePopup.classList.add('popup__opened');
+    editProfilePopup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     descriptionInput.value = profileDescription.textContent;
 });
 
 editProfilePopupCloseButton.addEventListener('click',() => {
-    editProfilePopup.classList.remove('popup__opened');
+    editProfilePopup.classList.remove('popup_opened');
 });
 
 editProfilePopupForm.addEventListener('submit', (event) => {
@@ -24,5 +24,5 @@ editProfilePopupForm.addEventListener('submit', (event) => {
     const description = descriptionInput.value;
     profileName.textContent = name;
     profileDescription.textContent = description;
-    editProfilePopup.classList.remove('popup__opened');
+    editProfilePopup.classList.remove('popup_opened');
 });
